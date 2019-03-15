5270d0edc62d6ddb8a86f9cb5b093468a99cc482
PierreN <pnolot@orange.fr>
2019-03-15 12:01:46 +0100

    Left menu add

--- View/php/template/Header.php
+++ View/php/template/Header.php
@@ -20,6 +20,8 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+    <!-- Test -->
+
     
     <title>LinkedOut</title>
 </head>
