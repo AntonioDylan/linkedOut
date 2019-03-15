39b8279c1b4432694e220d98c514a29b283fb4bf
MamieKalash <loris.d@hotmail.fr>
2019-03-15 14:16:52 +0100

    modeles suite

---
+++ Model/Entreprise.php
@@ -0,0 +1,33 @@
+<?php
+class Entreprise extends DBConnection {
+
+private $idEntreprise;
+private $nom;
+private $domaine;
+private $lienphoto;
+private $adresse;
+private $cp;
+private $ville;
+private $region;
+private $pays;
+private $description;
+private $site;
+
+
+function __construct(){
+    $this.idEntreprise = $idEntreprise;
+    $this.nom = $nom;
+    $this.domaine = $domaine;
+    $this.lienPhoto = $lienphoto;
+    $this.adresse = $adresse;
+    $this.cp = $cp;
+    $this.ville = $ville;
+    $this.region = $region;
+    $this.pays = $pays;
+    $this.description = $description;
+    $this.site = $site;
+
+}
+
+}
+?>
---
+++ Model/Recruteur.php
@@ -0,0 +1,14 @@
+<?php
+class Competence extends DBConnection {
+
+private $idRecruteur;
+private $idPersonne;
+
+
+function __construct(){
+    $this->idRecruteur = $idRecruteur;
+    $this->idPersonne = $idPersonne;
+}
+
+}
+?>
