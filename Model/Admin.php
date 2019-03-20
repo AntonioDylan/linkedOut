92950da787552405ae2428439136117766d54121
dylan antonio <dylan_83000@hotmail.fr>
2019-03-20 11:49:40 +0100

    add include db

--- Model/Admin.php
+++ Model/Admin.php
@@ -1,82 +1,82 @@
 <?php
-class Admin {
-
-private $id;
-private $login;
-private $password;
-
-
-
-
-
-
-function __construct($id, $login, $password){
-    $this.id = $id;
-    $this.login = $login;
-    $this.password = $password;
-}
-
-
-
-
-/**
- * Get the value of id
- */ 
-public function getId()
+class Admin
 {
-return $this->id;
-}
 
-/**
+    private $id;
+    private $login;
+    private $password;
+
+
+
+
+    function __construct($id, $login, $password)
+    {
+        $this->id = $id;
+        $this->login = $login;
+        $this->password = $password;
+    }
+
+
+
+
+    /**
+ * Get the value of id
+ */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
  * Set the value of id
  *
  * @return  self
- */ 
-public function setId($id)
-{
-$this->id = $id;
+ */
+    public function setId($id)
+    {
+        $this->id = $id;
 
-return $this;
-}
+        return $this;
+    }
 
-/**
+    /**
  * Get the value of login
- */ 
-public function getLogin()
-{
-return $this->login;
-}
+ */
+    public function getLogin()
+    {
+        return $this->login;
+    }
 
-/**
+    /**
  * Set the value of login
  *
  * @return  self
- */ 
-public function setLogin($login)
-{
-$this->login = $login;
+ */
+    public function setLogin($login)
+    {
+        $this->login = $login;
 
-return $this;
-}
+        return $this;
+    }
 
-/**
+    /**
  * Get the value of password
- */ 
-public function getPassword()
-{
-return $this->password;
-}
+ */
+    public function getPassword()
+    {
+        return $this->password;
+    }
 
-/**
+    /**
  * Set the value of password
  *
  * @return  self
- */ 
-public function setPassword($password)
-{
-$this->password = $password;
+ */
+    public function setPassword($password)
+    {
+        $this->password = $password;
 
-return $this;
+        return $this;
+    }
 }
-}
-?>
+ 
