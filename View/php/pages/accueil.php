f395afb2b0d7f868bb5a121680bb4ac0881a0578
MamieKalash <loris.d@hotmail.fr>
2019-03-21 16:23:20 +0100

    card

--- View/php/pages/accueil.php
+++ View/php/pages/accueil.php
@@ -1,6 +1,14 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/linkedOut/View/php/template/Header.php';
-    var_dump($_SESSION);
 ?>
 
-<h1>ACCUEIL</h1>
+
+<body>
+    <?php
+        include $_SERVER['DOCUMENT_ROOT'] . '/linkedOut/View/php/template/NavBar.php';
+        include $_SERVER['DOCUMENT_ROOT'] . '/linkedOut/View/php/template/LeftMenu.php';
+        $title = "olivier";
+        include $_SERVER['DOCUMENT_ROOT'] . '/linkedOut/View/php/template/Card.php';
+    ?>
+    <p> olivier </p>
+</body>
